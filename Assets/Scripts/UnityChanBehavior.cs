b293fed628ffa214e40833cad1aecb862ece1e47
Alice <alice.funk@laposte.net>
2017-10-12 16:12:37 +0200

    Ajout speechRecognition

--- Assets/Scripts/UnityChanBehavior.cs
+++ Assets/Scripts/UnityChanBehavior.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Leap;
 using Leap.Unity;
+using UnityEngine.Windows.Speech;
 
 public class UnityChanBehavior : MonoBehaviour
 {
+    private KeywordRecognizer keywordRecognizer;
+    protected PhraseRecognizer recognizer;
+    protected string word = null;
+
+    public string[] keywords = new string[] { "kick", "down", "left", "right" };
+    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
 
     Animator anim;
     [SerializeField] Controller controller;
@@ -20,6 +27,7 @@ public class UnityChanBehavior : MonoBehaviour
     public float speed = 6.0F;
     public float gravity = 20.0F;
 
+    int kickHash = Animator.StringToHash("kick");
     int jumpHash = Animator.StringToHash("Jump");
     int runStateHash = Animator.StringToHash("Base Layer.Locomotion");
 
@@ -30,8 +38,19 @@ public class UnityChanBehavior : MonoBehaviour
         //Hand hand = new Hand();
         charaController = gameObject.GetComponent<CharacterController>();
 
-        
+        if (recognizer != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+        }
 
+    }
+
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        word = args.text;
     }
 
     // Update is called once per frame
@@ -43,7 +62,14 @@ public class UnityChanBehavior : MonoBehaviour
 
         if (hand.PalmPosition.z + 1.45f * 50 > 0.4)
         {
+
             move = (hand.PalmPosition.z + 1.45f) * 100;
+            if(move < 0)
+            {
+                move = move / 50;
+            }
+
+
             anim.SetFloat("Speed", move);
             moveDirection = Vector3.forward*(move/100);
 
@@ -75,8 +101,35 @@ public class UnityChanBehavior : MonoBehaviour
         }
         else { anim.ResetTrigger(jumpHash); }
 
+        Debug.Log(word);
+        if (word != null)
+        {
+            switch (word)
+            {
+                case "kick":
+                    anim.SetTrigger(kickHash);
+                    break;
+                case "down":
 
+                    break;
+                case "left":
 
+                    break;
+                case "right":
+
+                    break;
+            }
+        }
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
     }
 
 
