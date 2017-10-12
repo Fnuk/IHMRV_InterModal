070c37ead53fe8d6f8010b36e3667ef1eb3db8d2
Alice <alice.funk@laposte.net>
2017-10-12 16:19:06 +0200

    SpeechRecognition for unity Chan

---
+++ Assets/Scripts/SpeechRecognition.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechRecognition : MonoBehaviour {
+
+    private KeywordRecognizer keywordRecognizer;
+    protected PhraseRecognizer recognizer;
+    protected string word = null;
+
+    public string[] keywords = new string[] { "kick", "down", "left", "right" };
+    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+
+    // Use this for initialization
+    void Start () {
+
+        if (recognizer != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+        }
+    }
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        word = args.text;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (word != null)
+        {
+            switch (word)
+            {
+                case "kick":
+                    anim.SetTrigger(kickHash);
+                    break;
+                case "down":
+
+                    break;
+                case "left":
+
+                    break;
+                case "right":
+
+                    break;
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+}
